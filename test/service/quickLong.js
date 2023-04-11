0732a034dcf313a37c9d77514f27874a97138f4c
bruce <610048251@qq.com>
2023-04-11 14:46:23 +0800

    update test for ftx

---
+++ test/service/quickLong.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+const XLSX = require('xlsx');
+
+const folderPath = path.resolve(__dirname, '../../data/quickLong'); // 将此路径替换为您的 CSV 文件夹路径
+
+// 读取文件夹中的所有文件名
+const files = fs.readdirSync(folderPath);
+
+// 筛选出 CSV 文件
+const csvFiles = files.filter(file => path.extname(file) === '.csv');
+
+// 初始化一个空数组来存储所有 CSV 文件的数据
+const combinedData = [];
+
+// 读取每个 CSV 文件并将其数据合并到 combinedData 数组中
+csvFiles.forEach(file => {
+  const csvFilePath = path.join(folderPath, file);
+  const csvFileContent = fs.readFileSync(csvFilePath, 'utf8');
+  const workbook = XLSX.read(csvFileContent, { type: 'string' });
+  const sheetName = workbook.SheetNames[0];
+  const worksheet = workbook.Sheets[sheetName];
+  const data = XLSX.utils.sheet_to_json(worksheet);
+  combinedData.push(...data);
+});
+
+function allFtxData() {
+  const sortresult = combinedData.sort((a, b) => {
+    Object.values(a)[2] - Object.values(b)[2]
+  })
+
+  let dataR = []
+
+  sortresult.forEach(e => {
+    dataR.push(e[Object.keys(e)[6]])
+  })
+
+  return dataR
+}
+
+module.exports = { allFtxData }
---
+++ test/testFtx.js
@@ -0,0 +1,50 @@
+const { getMonthlyCandles } = require('./service/bian')
+const { addNumbers, multiplyNumbers, subtractNumbers, divideNumbers } = require('./bignumber')
+// const { allFtxData } = require('./service/ftx')
+// const { allFtxData } = require('./service/volatility')
+// const { allFtxData } = require('./service/quickLong')
+const { allFtxData } = require('./service/quickShort')
+
+async function main() {
+  // const data = await getMonthlyCandles();
+  const data = allFtxData()
+  console.log("btc price  len ", data.length)
+  const results = [];
+  let sourceBalance = "1000"
+  let holderBtc = "0"
+
+  for (let i = 0; i < data.length; i++) {
+    // if () 波动比例大于2 个点 data 重新rebalance
+    const percentage = ((data[i] - data[i - 1]) / data[i]) * 100;
+    console.log(`我们计算到了第 ${i} 个, 波动比 ${percentage}`)
+    if (Math.abs(percentage) > 1) {
+      const [s, h] = myStrategy(data[i], sourceBalance, holderBtc, data[i - 1])
+      sourceBalance = s;
+      holderBtc = h
+      results.push(s);
+    }
+  }
+
+  const newR = results.sort((a, b) => a - b);
+  const newD = data.sort((a, b) => b - a);
+  console.log(`价格变化从 ${data[0]} 到 ${data[data.length - 1]}`)
+  console.log(`最后的盈利变化是 ${results[results.length - 1]} ${results[0]}`);
+  console.log(`最大价格为 ${newD[0]} , 最大盈利为 ${newR[newR.length - 1]}`)
+
+}
+function myStrategy(data, sourceBalance, holderBtc, lastDayPrice) {
+  console.log("sourceBalance   ", sourceBalance)
+  console.log("holderBtc    ", holderBtc)
+  // const yingli = data * holderBtc - lastDayPrice * holderBtc
+  const s = sourceBalance - ((sourceBalance - holderBtc * data) / 2)
+  // const h = holderBtc + ((sourceBalance - holderBtc * data) / 2) / data
+  const holderTimesData = multiplyNumbers(holderBtc, data)
+  const h = addNumbers(holderBtc, divideNumbers(divideNumbers(subtractNumbers(sourceBalance, holderTimesData), 2), data))
+  if (holderBtc = 0) {
+    return [500, 500 / data]
+  } else {
+    return [s, h]
+  }
+}
+
+main()
