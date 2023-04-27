29fda22bf25dfb2c5cc5bda0881e4a2c2001119b
bruce <610048251@qq.com>
2023-04-27 20:07:21 +0800

    fix

--- okex/okex_stddev_minute_close_price.js
+++ okex/okex_stddev_minute_close_price.js
@@ -3,33 +3,46 @@ const math = require('mathjs');
 
 const OKEX_API_URL = 'https://www.okex.com/api/v5/market/candles?instId={instrument_id}&after={start_time}&before={end_time}&bar=1m';
 const TWO_WEEKS_MS = 2 * 7 * 24 * 60 * 60 * 1000;
+const REQUEST_INTERVAL_MS = 6 * 60 * 60 * 1000; // 请求6小时的数据
 
 // 替换为您感兴趣的交易对，例如："BTC-USDT"
 const instrument_id = 'BTC-USDT';
 
+async function fetchCandles(startTime, endTime) {
+  const response = await axios.get(
+    OKEX_API_URL.replace('{instrument_id}', instrument_id)
+      .replace('{start_time}', startTime)
+      .replace('{end_time}', endTime)
+  );
+  return response.data.data;
+}
+
 async function calculateStdDevMinuteClosePrice() {
   try {
-    const startTime = Math.floor((Date.now() - TWO_WEEKS_MS) / 1000);
-    const endTime = Math.floor(Date.now() / 1000);
+    const startTimestamp = Math.floor((Date.now() - TWO_WEEKS_MS) / 1000);
+    const endTimestamp = Math.floor(Date.now() / 1000);
+    let currentTime = startTimestamp;
 
-    const response = await axios.get(
-      OKEX_API_URL.replace('{instrument_id}', instrument_id)
-        .replace('{start_time}', startTime)
-        .replace('{end_time}', endTime)
-    );
-    const data = response.data.data;
+    let allData = [];
 
-    if (!data || data.length === 0) {
-      console.error('No data received from OKEx API.');
-      console.log(response.data);
-      return;
+    while (currentTime < endTimestamp) {
+      const nextTime = Math.min(currentTime + REQUEST_INTERVAL_MS / 1000, endTimestamp);
+      const data = await fetchCandles(currentTime, nextTime);
+
+      if (!data || data.length === 0) {
+        console.error('No data received from OKEx API for time range:', currentTime, nextTime);
+        return;
+      }
+
+      allData = allData.concat(data);
+      currentTime = nextTime;
     }
 
-    const closePrices = data.map(item => parseFloat(item[4]));
+    const closePrices = allData.map(item => parseFloat(item[4]));
 
     if (closePrices.length === 0) {
       console.error('Close prices array is empty.');
-      console.log(data);
+      console.log(allData);
       return;
     }
 
