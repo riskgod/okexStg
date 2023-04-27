a4ff0634dac50385db0c721d354a519614d15720
bruce <610048251@qq.com>
2023-04-27 20:24:34 +0800

    fix

--- okex/topToken.js
+++ okex/topToken.js
@@ -1,33 +1,40 @@
 const axios = require('axios');
 
-const COINGECKO_API_URL = 'https://api.coingecko.com/api/v3/exchanges/okex';
-const TWO_WEEKS_MS = 2 * 7 * 24 * 60 * 60 * 1000; // 两周的毫秒数
+const OKEX_API_URL = 'https://www.okex.com/api/v5/market/candles?instId={instrument_id}&startTime={start_time}&endTime={end_time}&bar=1H';
+const TWO_WEEKS_MS = 2 * 7 * 24 * 60 * 60 * 1000;
 
-async function getTopTwentyUsdtTokens() {
+// 替换为您感兴趣的交易对，例如："BTC-USDT"
+const instrument_id = 'BTC-USDT';
+
+async function fetchCandles(startTime, endTime) {
+  const response = await axios.get(
+    OKEX_API_URL.replace('{instrument_id}', instrument_id)
+      .replace('{start_time}', startTime)
+      .replace('{end_time}', endTime)
+  );
+  return response.data.data;
+}
+
+async function calculateTotalTradingVolume() {
   try {
-    const response = await axios.get(COINGECKO_API_URL);
-    const data = response.data.tickers;
+    const startTimestamp = Math.floor((Date.now() - TWO_WEEKS_MS) / 1000);
+    const endTimestamp = Math.floor(Date.now() / 1000);
 
-    // 过滤USDT交易对和过去两周的数据
-    const usdtTwoWeeksData = data.filter(item => {
-      const timestamp = new Date(item.last_fetch_at).getTime();
-      return item.target === 'USDT' && Date.now() - timestamp <= TWO_WEEKS_MS;
-    });
+    const data = await fetchCandles(startTimestamp, endTimestamp);
 
-    // 根据交易量对数据进行排序
-    const sortedData = usdtTwoWeeksData.sort((a, b) => parseFloat(b.trade_volume_24h_btc_normalized) - parseFloat(a.trade_volume_24h_btc_normalized));
+    if (!data || data.length === 0) {
+      console.error('No data received from OKEx API.');
+      return;
+    }
 
-    // 获取排名前20的Token
-    const topTwentyTokens = sortedData.slice(0, 20).map(item => ({
-      symbol: item.base + '/' + item.target,
-      tradingVolume: item.trade_volume_24h_btc_normalized,
-    }));
+    const tradingVolumes = data.map(item => parseFloat(item[5]));
+    const totalTradingVolume = tradingVolumes.reduce((sum, volume) => sum + volume, 0);
 
-    console.log('Top 20 USDT Tokens by Trading Volume in the Last 2 Weeks:');
-    console.log(topTwentyTokens);
+    console.log(`Total Trading Volume for ${instrument_id} in the Last 2 Weeks:`);
+    console.log(totalTradingVolume);
   } catch (error) {
     console.error(`Error fetching data: ${error}`);
   }
 }
 
-getTopTwentyUsdtTokens();
+calculateTotalTradingVolume();
