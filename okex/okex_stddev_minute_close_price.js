fbac077ee3adc3b6565d58f2a2a88460876c526d
bruce <610048251@qq.com>
2023-04-27 20:09:44 +0800

    fix

--- okex/okex_stddev_minute_close_price.js
+++ okex/okex_stddev_minute_close_price.js
@@ -3,7 +3,8 @@ const math = require('mathjs');
 
 const OKEX_API_URL = 'https://www.okex.com/api/v5/market/candles?instId={instrument_id}&after={start_time}&before={end_time}&bar=1m';
 const TWO_WEEKS_MS = 2 * 7 * 24 * 60 * 60 * 1000;
-const REQUEST_INTERVAL_MS = 6 * 60 * 60 * 1000; // 请求6小时的数据
+const REQUEST_INTERVAL_MS = 1 * 60 * 60 * 1000; // 请求1小时的数据
+const REQUEST_DELAY_MS = 200; // 200毫秒的请求延迟
 
 // 替换为您感兴趣的交易对，例如："BTC-USDT"
 const instrument_id = 'BTC-USDT';
@@ -15,6 +16,10 @@ async function fetchCandles(startTime, endTime) {
       .replace('{end_time}', endTime)
   );
   return response.data.data;
+}
+
+async function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function calculateStdDevMinuteClosePrice() {
@@ -36,6 +41,8 @@ async function calculateStdDevMinuteClosePrice() {
 
       allData = allData.concat(data);
       currentTime = nextTime;
+
+      await sleep(REQUEST_DELAY_MS);
     }
 
     const closePrices = allData.map(item => parseFloat(item[4]));
